a99ea0cd63b760c2ee1e4ac951f317fc4c045b31
vloeiolzlr <yoonseed1995@naver.com>
2025-06-16 11:08:58 +0900

    최종 수정 완료

--- spring/src/main/java/com/dmubackend/spring/domain/mail/scheduler/MailScheduler.java
+++ spring/src/main/java/com/dmubackend/spring/domain/mail/scheduler/MailScheduler.java
@@ -3,6 +3,7 @@ package com.dmubackend.spring.domain.mail.scheduler;
 import com.dmubackend.spring.domain.mail.service.MailService;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -12,8 +13,9 @@ import java.io.IOException;
 public class MailScheduler {
     private final MailService mailService;
 
-   //@Scheduled(cron = "0 * * * * *")
+    //@Scheduled(cron = "0 */3 * * * *")
     public void mailScheduler() throws MessagingException, IOException {
         mailService.sendMailUser(null);
+        System.out.println("구독자 메일 발송 완료");
     }
 }
